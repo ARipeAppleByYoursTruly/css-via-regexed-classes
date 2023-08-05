d4db003937039ca7bcb687b241a73c4ef667c409
ARipeAppleByYoursTruly <appleandroid1104@gmail.com>
2023-08-05 16:13:39 +0800

    Upload files and update README

--- README.md
+++ README.md
@@ -1,2 +1,157 @@
-# css-via-regexed-classes
- A script that generates a CSS file, heavily inspired by UnoCSS (please use UnoCSS instead, unless you know what you're doing)
+# CSS via Regex-ed Classes
+
+## What is this?
+
+Heavily inspired by UnoCSS, this is a script that extract class names from a project and generates a CSS file with styles as specified by rules, variants, shortcuts, and layers defined by the developer.
+
+In actuality, this is a less advanced and less flexible version of UnoCSS. Like, it's not even a `npm` package, it's just one script, and you need to manually set it up. If you're satisfied in using Tailwind CSS, UnoCSS, or any other CSS frameworks and language supercedes, then don't use this.
+
+To deter you even further from using this script, you have to modify the script itself if you want to change its behaviour, though I did try so make some stuff "configurable". A considerable amount of technical prowess is required, but at least it doesn't involve having 10 years experience in Vite plugin and `npm` package development. What an agonizing experience that was.
+
+
+
+## Then why does this exists?
+
+Because UnoCSS didn't provide a way to alter the order of generated styles.
+
+Imagine you have a button with the classes `:hover?color:red` and `:active?color:green` (I believe it's obvious what styles they apply). Logically, `:hover?color:red` should be placed before `:active?color:green` in the CSS file, but `a` comes before `h`, so UnoCSS places `:active?color:green` before `:hover?color:red`, and now you got a visual bug of the button having no onClicked state even though it functions correctly.
+
+
+
+## How to use this?
+
+### Requirements
+
+-   Node.js
+-   `npm` packages: (`npm i -D <package>`)
+    -   fast-glob
+    -   cssesc
+    -   @iconify/utils (optional)
+    -   @iconify/json (optional)
+
+
+
+### Setup
+
+1.  Place the script somewhere in the project that won't get included in production
+2.  In `packge.json`, add a new key-value pair in `scripts`
+    ```json
+    // ...
+    "scripts": {
+      // ...
+      "<unique command name>": "node ./<path to the script from project root>"
+      // Example: "generateCSS": "node ./tools/css-via-regexed-classes.js"
+
+      // ...
+    }
+    ```
+
+3.  Add new "pre-" command key-value pairs to `scripts` to commands that will build or run your project
+    ```json
+    // Example: For Astro projects, add "predev" and "prebuild" key-value pairs to "scripts"
+
+    // ...
+    "scripts": {
+      // ...
+      "predev": "npm run <unique command name>",
+      "dev": "astro dev",
+      // ...
+      "prebuild": "npm run <unique command name>",
+      "build": "astro build",
+      // ...
+    }
+    ```
+
+4.  Since this is just a script, you can modify the code directly to:
+    -   Add glob patterns to specify the files to perform class name extraction on
+    -   Modify the default class name extraction logic `(advanced)`
+    -   Add shortcuts
+    -   Modify the default variant handling logic `(advanced)`
+    -   Add rules (and/or not use Iconify icons)
+    -   Add layers
+    -   Alter the final order of the generated styles to handle special cases `(slightly advanced)`
+    -   Enable or disable style merging for shortcuts
+    -   Specify the filepath of the output CSS file
+
+5.  Finally, to use the generated styles, import the CSS file to your webpages
+
+
+
+## Default Behavior out of the Box
+
+### Class Name Extraction Logic
+
+The default logic is to match `class=...` in every starting HTML tags and extract the values of the `class` attribute.
+
+The way to extract class names depends on the characters used to wrap the value:
+-   `'...'` and `"..."` - Split the value by "` `" and extract each token
+-   `` `...` `` - Similar to the case above, but also need to check the values of any `${}` within
+-   `{...}` (and `${...}` from `` `...` ``) - Match any `'...'` and `"..."` but ignore the ones used in comparisons (of ternary operators), also need to check the values of any `` `...` `` within
+
+
+
+### Variants
+
+Variants serve as ways to handle CSS' combinators and pseudo selectors, other stuff like at-rules aren't implemented as of now.
+
+Syntax: `<variant(s)>?<rule>`
+
+Example usage: `>div:hover?color:red` - There are 2 variants here, `><target>` and `:<target>`
+
+Current variants:
+-   `_<target>` - Descendent combinator, from `button div`
+-   `~<target>` - Sibling combinator, from `button ~ div`
+-   `><target>` - Child combinator, from `button > div`
+-   `+<target>` - Adjacent combinator, from `button + div`
+-   `:<target>` - Pseudo-class selector, from `button:hover`
+-   `::<target>` - Pseudo-element selector, from `button::before`
+
+
+
+### Rules
+
+-   `icon_<collection-name>_<icon-name>` - To use pure CSS icons from Iconify, example: `icon_material-symbols_light-mode`
+-   `<css-property>:<value_with_or_without_spaces>` - The rule that basically enables writing CSS in the `class` attribute, example: `margin:10px_10px_0`, `background-color:var(--background)`
+    Please remember that "` `" seperates classes, so values that requires "` `" to work need to use "`_`" instead
+
+
+### Generated Styles Sorting Logic
+
+The default logic will first categorize the generated styles into their respective layers.
+
+Layers, in order from lowest priority to highest:
+-   `icons`
+-   `shortcuts`
+-   `atomics`
+
+Then, in every layer, each generated style will be sorted via JavaScript's `localeCompare()`.
+
+The logic will check the sorted styles again, to handle special cases. I've only implemented one special case of sorting `:hover` before `:active`.
+
+
+
+### Style Merging Logic
+
+Enabled by default, the logic will first check if it's currently writing styles in the `shortcuts` layer.
+
+Then it will check the style's selector of current index and next index. If both selectors are identical, add the latter's body to the current's body. Otherwise, write the style with the now merged body into the file.
+
+
+
+## Final Words
+
+I wanna pass you the Olympian torch to continue the chain of innovation.
+
+This script is definitely flawed in many ways, but I do hope that it's easier to understand than UnoCSS' customization, at least for beginners in the web development landscape. I say that because currently, UnoCSS' documentation lacks many details, which require users to dive deep into the repo. And that repo contains such advanced logic and split it into so many files that it'll make beginners' head spin.
+
+This script also kinda maybe helps beginners bridge the gap from vanilla CSS to atomic CSS, I think. Since the syntax used is mostly from CSS itself, and not framework-specific, like Tailwind CSS.
+
+As for professionals that stumble upon this insignificant repo, I'm still gonna direct you UnoCSS instead. And if possible, contribute to UnoCSS. I think UnoCSS has a lot of potential, but it do seem to target a niche audience. As I said before, UnoCSS is like the Vim of CSS... frameworks? Engines? Libraries? What's the umbrella term for frameworks, engines, libraries, etc?
+
+If you somehow want to improve this little thing I made instead of UnoCSS, MIT license, make another framework. The world has enough JS frameworks, but not enough CSS frameworks. This script lacks so many stuff compared to UnoCSS, a proper CSS ~~framework~~ engine, it feels nonsensical to make improvements for this script.
+
+I thought I'm niche enough already, but apparently, I can be even more niche, using my own way to generate CSS styles instead of UnoCSS, Tailwind CSS, or even vanilla CSS.
+
+Also, I owe Anthony Fu and the UnoCSS team a big thank you for doing many parts of the brainstorming process. I wouldn't even have the idea of making this script if it weren't for UnoCSS' existence. I borrowed a lot of ideas from it.
+
+Thank you for your time in reading this README.
---
+++ project/css-via-reggexed-classes.js
@@ -0,0 +1,569 @@
+import fg from "fast-glob"
+import fs from "node:fs"
+import cssesc from "cssesc"
+
+// Comment this out if you don't plan to use Iconify icons
+import { getIconData, getIconCSS } from "@iconify/utils"
+
+
+
+// Configurable stuff
+// ===
+const files = fg.sync([
+  // Add glob patterns here
+
+  // The patterns should match files that has HTML elements in them
+  // Like .html, .astro, .jsx, .tsx, and more that I don't know
+
+  // Example, for Astro projects:
+  "./src/**/*.astro",
+  "./src/**/*.tsx"
+])
+
+const layers = [
+  // The order of layers here reflects the output of the css file
+
+  // Example:
+  "icons", // Comment this out if you don't plan to use Iconify icons
+  "shortcuts",
+  "atomics"
+]
+
+// Self-explanatory
+const outputFilePath = "./src/styles/generated-css-test.css"
+
+
+
+// Extract class names from codebase
+// ===
+// I hope you know your stuff if you modify this section
+console.log("Extracting class names ...")
+
+let extractedClassNames = []
+
+
+
+files.forEach(file => {
+  const fileContent = fs.readFileSync(file, "utf-8")
+
+  // This regex only matches the starting HTML tags
+  // This also matches "if (a < b || a > c) {", but will be discarded by const classAttribute
+  const htmlTags = fileContent.match(/<[^</>]+>/g)
+
+  if (htmlTags === null) {
+    return
+  }
+
+  htmlTags.forEach(htmlTag => {
+    // Normalizes all whitespace to single spaces
+    const singleLineHtmlTag = htmlTag.replaceAll(/\s+/g, " ")
+
+    // Note: Solid also has the classList attribute, so need to deal with that eventually
+    const classAttribute = singleLineHtmlTag.match(/class=('.+'|".+"|{.+})/)
+
+    if (classAttribute === null) {
+      return
+    }
+
+    // {} can contain ``, '' or ""
+    // `` can contain ${}
+
+    // Potential recursion from {} and `` containing each other, but unrealistic
+
+    // Having multiple ${} in `` will add a layer of complexity
+    // But I've decided to concatenate them to one ${}, not sure if this is a good solution
+    let classValue = classAttribute[1]
+
+    while (true) {
+      if (classValue.startsWith("{")) {
+        classValue = classValue.substring(1, classValue.length - 1).trim()
+
+
+
+        let toBeCheckedAgain = classValue.match(/`[^`]+`/g)
+
+        if (toBeCheckedAgain !== null) {
+          toBeCheckedAgain.forEach((result, i) => {
+            classValue = classValue.replace(result, "")
+
+            toBeCheckedAgain[i] = result.substring(1, result.length - 1).trim()
+          })
+        }
+
+
+
+        // This regex is made assuming that every whitespace has been reduced to a single space
+        // in singleLineHtmlTag
+
+        // To avoid matching strings that are used for comparison
+        // eg. var === "won't be matched" ? "class1" : "class2"
+        let toBeAdded = classValue.match(/'(?!\s\?\s)[^']+'(?!\s\?)|"(?!\s\?\s)[^"]+"(?!\s\?)/g)
+
+        if (toBeAdded !== null) {
+          toBeAdded.forEach(result => {
+            extractedClassNames.push(...result.substring(1, result.length - 1).trim().split(" "))
+          })
+        }
+
+
+
+        if (toBeCheckedAgain !== null) {
+          classValue = "`"
+
+          toBeCheckedAgain.forEach(result => {
+            classValue += `${result} `
+          })
+
+          classValue = `${classValue.trim()}\``
+        }
+      }
+
+
+
+      else if (classValue.startsWith("`")) {
+        classValue = classValue.substring(1, classValue.length - 1).trim()
+
+
+
+        let toBeCheckedAgain = classValue.match(/\${[^$]+}/g)
+
+        if (toBeCheckedAgain !== null) {
+          toBeCheckedAgain.forEach((result, i) => {
+            classValue = classValue.replace(result, "")
+
+            toBeCheckedAgain[i] = result.substring(2, result.length - 1).trim()
+          })
+        }
+
+
+
+        let toBeAdded = classValue.trim().split(" ")
+
+        if (toBeAdded !== null) {
+          extractedClassNames.push(...toBeAdded)
+        }
+
+
+
+        // The aforementioned ${} concatenation
+        // ${...} ${...}   will become   ${... ...}
+        if (toBeCheckedAgain !== null) {
+          classValue = "{"
+
+          toBeCheckedAgain.forEach(result => {
+            classValue += `${result} `
+          })
+
+          classValue = `${classValue.trim()}}`
+        }
+      }
+
+
+
+      else if (classValue.startsWith("'") || classValue.startsWith("\"")) {
+        classValue = classValue.substring(1, classValue.length - 1).trim()
+
+        let toBeAdded = classValue.trim().split(" ")
+
+        if (toBeAdded !== null) {
+          extractedClassNames.push(...toBeAdded)
+        }
+      }
+
+
+
+      else {
+        // console.log("=============================")
+        // console.log("Infinite Loop Exit Checkpoint")
+        // console.log("=============================")
+        // console.log("htmlTag")
+        // console.log(htmlTag)
+        // console.log()
+        // console.log("extractedClassNames")
+        // console.log(extractedClassNames)
+        // console.log("=============================\n")
+
+        break
+      }
+    }
+  })
+})
+
+console.log("Done")
+
+
+
+// Generate styles based on the extracted class names
+// ===
+console.log("Generating styles ...")
+
+function createStylingObject() {
+  return {
+    className: "",
+    isShortcut: false,
+    layer: "",
+    rules: [],
+    generatedStyles: []
+  }
+}
+
+let stylingObjects = []
+
+
+
+// Check if the class name matches a shortcut
+// ---
+stylingObjects = extractedClassNames.map(extractedClassName => {
+  let obj = createStylingObject()
+  obj.className = extractedClassName
+
+  let shortcutMatch = ""
+
+
+
+  // Each logical section handles a shortcut
+
+  // Template
+  /*
+  shortcutMatch = extractedClassName.match(/your shortcut regex here/)
+
+  if (shortcutMatch !== null) {
+    obj.isShortcut = true
+    obj.layer = "shortcuts"
+    obj.rules = [
+      // Your rules here
+    ]
+
+    return obj
+  }
+  */
+
+  shortcutMatch = extractedClassName.match(/^button-icon$/)
+
+  if (shortcutMatch !== null) {
+    obj.isShortcut = true
+    obj.layer = "shortcuts"
+    obj.rules = [
+      "width:50px",
+      "height:50px",
+      ">div:hover?color:red",
+      ">div:active?color:purple"
+    ]
+
+    return obj
+  }
+
+
+
+  shortcutMatch = extractedClassName.match(/^devonly-bg-on-all:(.+)$/)
+
+  if (shortcutMatch !== null) {
+    obj.isShortcut = true
+    obj.layer = "shortcuts"
+    obj.rules = [
+      `background-color:${shortcutMatch[1]}`,
+      `_*?background-color:${shortcutMatch[1]}`
+    ]
+
+    return obj
+  }
+
+
+
+  // Not a shortcut
+  obj.rules.push(extractedClassName)
+
+  return obj
+})
+
+
+
+// Build the selector for each rule (handling variants)
+// ---
+function createStyle() {
+  return {
+    selector: "",
+    body: ""
+  }
+}
+
+
+
+// I hope you know your stuff if you modify this section
+stylingObjects.forEach(stylingObject => {
+  stylingObject.generatedStyles = stylingObject.rules.map(rule => {
+    let style = createStyle()
+
+    // Escapes special characters to be used as the selector
+    style.selector = `.${cssesc(stylingObject.className, { isIdentifier: true })}`
+
+    let variants = rule.match(/^(.+)\?/)
+
+    if (variants === null) {
+      return style
+    }
+
+    variants = variants[1]
+
+    let variantRegex = /^$/
+    let variantTransformation = ""
+
+    while (true) {
+      if (variants.startsWith("_")) {
+        variantRegex = /^_([^_~>+|:]+)/
+        variantTransformation = " "
+      }
+      else if (variants.startsWith("~")) {
+        variantRegex = /^~([^_~>+|:]+)/
+        variantTransformation = " ~ "
+      }
+      else if (variants.startsWith(">")) {
+        variantRegex = /^>([^_~>+|:]+)/
+        variantTransformation = " > "
+      }
+      else if (variants.startsWith("+")) {
+        variantRegex = /^\+([^_~>+|:]+)/
+        variantTransformation = " + "
+      }
+      else if (variants.startsWith("|")) {
+        variantRegex = /^\|([^_~>+|:]+)/
+        variantTransformation = " | "
+      }
+      else if (variants.startsWith("::")) {
+        variantRegex = /^::([^_~>+|:]+)/
+        variantTransformation = "::"
+      }
+      else if (variants.startsWith(":")) {
+        variantRegex = /^:([^_~>+|:]+)/
+        variantTransformation = ":"
+      }
+      else {
+        // console.log("=============================")
+        // console.log("Infinite Loop Exit Checkpoint")
+        // console.log("=============================")
+        // console.log("rule")
+        // console.log(rule)
+        // console.log()
+        // console.log("style.selector")
+        // console.log(style.selector)
+        // console.log("=============================\n")
+
+        break
+      }
+
+      const variantMatch = variants.match(variantRegex)
+
+      if (variantMatch !== null) {
+        style.selector += `${variantTransformation}${variantMatch[1]}`
+
+        variants = variants.replace(variantMatch[0], "")
+      }
+    }
+
+    return style
+  })
+})
+
+
+
+// Build the body of each rule
+stylingObjects.forEach(stylingObject => {
+  stylingObject.rules.forEach((rule, index) => {
+    // Remove variants as they have been handled already
+    if (rule.indexOf("?") > 0) {
+      rule = rule.substring(rule.indexOf("?") + 1)
+    }
+
+    let ruleMatch = []
+
+
+
+    // Each rule syntax has priority ranking, from highest to lowest
+    // Template:
+    /*
+    ruleMatch = rule.match(<your rule regex>)
+
+    if (ruleMatch !== null) {
+      // perform stuff
+
+      stylingObject.generatedStyles[index].body = <the result style body>
+
+      if (stylingObject.layer === "") {
+        stylingObject.layer = <the layer for your rule>
+      }
+
+      return
+    }
+    */
+
+    // Comment out this rule if you don't plan to use Iconify icons
+    ruleMatch = rule.match(/^icon_(.+)_(.+)$/)
+
+    if (ruleMatch !== null) {
+      const iconifyData = getIconData(
+        JSON.parse(fs.readFileSync(`./node_modules/@iconify/json/json/${ruleMatch[1]}.json`)),
+        ruleMatch[2]
+      )
+
+      const iconifyCSS = getIconCSS(iconifyData)
+
+      const iconifyCSSBody = iconifyCSS.substring(
+        iconifyCSS.indexOf("{") + 1,
+        iconifyCSS.indexOf("}")
+      )
+
+      stylingObject.generatedStyles[index].body = `  ${iconifyCSSBody.trim()}`
+
+      if (stylingObject.layer === "") {
+        stylingObject.layer = "icons"
+      }
+
+      return
+    }
+
+
+
+    ruleMatch = rule.match(/^(.+):(.+)$/)
+
+    if (ruleMatch !== null) {
+      const CSSValue = ruleMatch[2].replaceAll("_", " ")
+
+      stylingObject.generatedStyles[index].body = `  ${ruleMatch[1]}: ${CSSValue};`
+
+      if (stylingObject.layer === "") {
+        stylingObject.layer = "atomics"
+      }
+
+      return
+    }
+  })
+})
+
+console.log("Done")
+
+
+
+// Sort the generated styles
+// ===
+console.log("Sorting styles ...")
+
+// Create an empty array for each layer
+let styleSheetObject = {}
+
+layers.forEach(layer => {
+  styleSheetObject[layer] = []
+})
+
+
+
+// Sort by layers
+stylingObjects.forEach(stylingObject => {
+  if (Object.hasOwn(styleSheetObject, stylingObject.layer)) {
+    styleSheetObject[stylingObject.layer].push(...stylingObject.generatedStyles)
+  }
+})
+
+
+
+// Sort by selectors
+for (const layer in styleSheetObject) {
+  styleSheetObject[layer].sort((a, b) => {
+    return a.selector.localeCompare(b.selector)
+  })
+}
+
+
+
+// Handle special cases
+for (const layer in styleSheetObject) {
+  styleSheetObject[layer].forEach((style, index) => {
+    // I hope you know your stuff if you modify this section
+    // Each case is a if statement
+
+    // Place :hover before :active
+    if (
+      style.selector.endsWith(":active") &&
+      style.selector[-8] !== "\\" &&
+      styleSheetObject[layer].length > index + 1 &&
+      styleSheetObject[layer][index + 1].selector.endsWith(":hover") &&
+      styleSheetObject[layer][index + 1].selector[-7] !== "\\"
+    ) {
+      let temp = styleSheetObject[layer][index]
+
+      styleSheetObject[layer][index] = styleSheetObject[layer][index + 1]
+      styleSheetObject[layer][index + 1] = temp
+    }
+  })
+}
+
+console.log("Done")
+
+
+
+// Write sorted styles to a file
+// ===
+const outputFileStream = fs.createWriteStream(outputFilePath)
+
+console.log(`Writing styles to ${outputFilePath} ...`)
+
+
+
+outputFileStream.write("/* Generated by css-via-regexed-classes.js */\n")
+
+for (const layer in styleSheetObject) {
+  if (styleSheetObject[layer].length <= 0) {
+    continue
+  }
+
+  outputFileStream.write(`\n\n\n/* Layer: ${layer} */`)
+  outputFileStream.write("\n/* === */")
+
+
+
+  // I hope you know your stuff if you modify this section
+
+  // To disable merging, comment out the rest of the code block and uncomment the following
+  /*
+  styleSheetObject[layer].forEach((style) => {
+    outputFileStream.write(`\n${style.selector} {\n${style.body}\n}\n`)
+  })
+  */
+  if (layer !== "shortcuts") {
+    styleSheetObject[layer].forEach((style) => {
+      outputFileStream.write(`\n${style.selector} {\n${style.body}\n}\n`)
+    })
+
+    continue
+  }
+
+  // Merge shortcut rules
+  let i = 0
+  let selector = styleSheetObject[layer][0].selector
+  let body = styleSheetObject[layer][0].body
+
+  while (true) {
+    if (i >= styleSheetObject[layer].length - 1) {
+      outputFileStream.write(`\n${selector} {\n${body}\n}\n`)
+
+      break
+    }
+
+    if (selector === styleSheetObject[layer][i + 1].selector) {
+      body += `\n${styleSheetObject[layer][i + 1].body}`
+    }
+    else {
+      outputFileStream.write(`\n${selector} {\n${body}\n}\n`)
+
+      selector = styleSheetObject[layer][i + 1].selector
+      body = styleSheetObject[layer][i + 1].body
+    }
+
+    i++
+  }
+}
+
+console.log("Done")
+
+
+
+console.log("\nPlease double-check the generated .css file, thank you\n")
