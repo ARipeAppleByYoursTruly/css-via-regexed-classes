666c1a27b08a4aae6228cb1234448cbd14aaa346
ARipeAppleByYoursTruly <appleandroid1104@gmail.com>
2023-08-08 14:11:28 +0800

    Create astro-plugin.ts
    Made an Astro plugin so that it will auto-regenerate CSS whenever a file changes, during dev server only

---
+++ source/astro-plugin.ts
@@ -0,0 +1,41 @@
+import type { AstroIntegration } from "astro"
+import child_process from "node:child_process"
+
+
+
+// To use this plugin, modify your astro.config.mjs like so:
+/*
+// ...
+import generateCSS from "filepath to astro-plugin.ts"
+// ...
+export default defineConfig({
+  integrations: [
+    // ...
+    generateCSS()
+    // ...
+  ]
+})
+*/
+
+export default function generateCSS(): AstroIntegration {
+  return {
+    name: "generate-css-via-regexed-classes",
+    hooks: {
+      "astro:server:setup": ({ server }) => {
+        server.watcher.on("change", (path) => {
+          // The generatede CSS file by css-via-regexed-classes.js
+          // To prevent infinite loop
+          if (path.endsWith("generated-via-regexed-classes.css")){
+            return
+          }
+
+          // Only generate CSS if specific files are changed
+          if (path.endsWith(".astro") || path.endsWith(".tsx")) {
+            // Replace "generate-css" with your command defined in package.json
+            child_process.exec("npm run generate-css")
+          }
+        })
+      }
+    }
+  }
+}
